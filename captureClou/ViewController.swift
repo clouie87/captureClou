49384e7266bb1551a8fbc68d4929058fdd94fcd5
clouie87 <carol.louie@gmail.com>
2015-05-07 16:30:33 -0400

    Initial Commit

---
+++ captureClou/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  captureClou
+//
+//  Created by Caroline Louie on 2015-05-07.
+//  Copyright (c) 2015 Caroline Louie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
